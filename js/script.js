b0e1c45dba217a979e5274b788c028f0feadf599
shutarou-kojima <shutarou.kojima@gmail.com>
2021-08-22 11:19:14 +0900

    追加　カウント処理の前後にHook追加　Counterクラス

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 class Counter {
-  constructor(duration) {
+  constructor(duration, preHook, postHook) {
     this.duration = Number.isInteger(duration) ? duration : 0;
     this.count;
     this.counterID;
     this.past;
     this.delay = 1000;
+    this.preHook = preHook;
+    this.postHook = postHook;
   }
 
   getCount() {
@@ -15,8 +17,15 @@ class Counter {
   updateDuration() {
     let now = Date.now();
     let diff = now - this.past;
+
+    // preHook 処理前に、条件付けでキャンセルする場合などに用いる。
+    this.preHook(now, diff);
+
     this.duration += diff;
     this.past = now;
+
+    // postHook 処理後に、Viewの更新やLocalStorageへ保存する場合などに用いる。
+    this.postHook(this.duration, this.getCount());
   }
 
   start() {
